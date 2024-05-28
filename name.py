1aea3c29c7dc7ab59e4f6058fbaade64c9fbe39b
Crissyactress <christalriziki@gmail.com>
2024-05-29 01:31:11 +0300

    added some few lines

--- name.py
+++ name.py
@@ -1,2 +1,7 @@
-x = input ('Enter Your Name: ')
-print ("Hello", x)
+name = input('what is your name ?')
+print = ('Hello', name)
+if name < 2:
+    print ('error, Try Again', name)
+if name  > 4:
+    print ('Welcome to Crissy World', name)
+    print ('finis')
